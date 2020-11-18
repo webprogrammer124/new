ff078d34d131151ada5c278c20db0eee249ee6de
Seema Yousuf Zai <72159369+webprogrammer124@users.noreply.github.com>
2020-11-18 11:59:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,102 @@
+//advance 16 nov 20//
+//if(true){
+    //let X=1:
+
+
+//let age = prompt("What is your age?", 18);
+
+//let welcome = (age < 18) ?
+ // () => alert('Hello') :
+ // () => alert("Greetings!");
+ //let multi = function(a, b) {
+   // return a * b;
+ // };
+  
+  
+  //alert( mult(1, 2) ); // 3 
+  //const multiply =()=>{
+     // var c =2*2;
+     // return (c);}
+  //console.log(multiply());
+  //map funtion saquare//
+ // var arr =[ 4,9,16,25];
+  //const newArr = arr.map(
+     //(item) =>{
+       //   return Math.sqrt(item)
+      //}
+
+ // )
+ // console.log(newArr)
+  //for greater number//
+  //var arr = [2,4,6,7];
+ // const newArr = arr.filter(
+     // (item) =>{
+        //  return item > 2
+     // }
+ // ) 
+ // console.log(newArr)
+ //17 nov 2020//
+// [a,b, c, d, ...e] = [10, 20,30,40,5 ,7,8];
+ //console.log(d)
+ //[a,b, c, d, ...e] = [10, 20,30,40,5 ,7,8];
+ //console.log(e)
+ //class task//
+ //var teachers =['missfaiza','sir ali','sir mughal']
+ //var ages = [20,18,19,21,23];
+ //var studnts = ['simee','bela','fati']
+ //let find = teachers.find((item)=>{
+  // return item =='missfaiza'
+ //})
+ //if(find){
+  // let res = studnts.map((item,index)=>{
+   //  return {name:item, age:ages[index]}
+   //})
+  //  console.log(res)
+ //}//terninat
+ //let age = 18;
+  //var cnic =( age>=18)?'yes': 'no'
+  //console.log(cnic)
+ // var str ='after break'
+ /// var learn =(str == 'after break')?'no':'yes'
+  //console.log(learn)
+  //class task//
+  //var salary = 50000
+ // var bounus =( salary>=5000)?'1000':'100'
+  //console.log(bounus)
+ // var allowed = (age>18) ?'yes'
+            //   :(age == 18)? 'let me think'
+             //  :(age == 18)? 'let me think' 
+          //     :invalid
+ // console.log(allowed)             
+//var per = 80
+//var solarship = (per>=80)?'you have solarship':
+//18 n0v 20 spread operator//
+
+//var arr =[2,3,4,5]
+//var arr2 = arr;
+//arr2[0] = 4;
+//console.log(arr)
+
+ // var arr = [2,3,4,5]
+ // var arr1 = [6,7,8,9]
+ // var arr2 = [...arr]
+ // arr2[0] =4
+ // console.log(arr2)
+  //concat//
+  //var arr = [2,3,4,5]
+  //var arr3 =[...arr,...arr1]
+  //console.log(arr3)
+ //object//
+ //var obj ={name:'faiza'}
+ //var objNew = {...obj};
+ //objNew.deal = 2;
+ //console.log(obj)
+ //function sum(x,y,z){
+    //return x+y+z
+ //}
+ //sum(3,5,6);
+ let arr4 = [3,5,6]
+ var res = sum(...arr4)
+ console.log(res)
+ 
+
